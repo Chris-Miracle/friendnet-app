bac69d25a8570aec6a764b6afe1275e22306e766
Chris-Miracle <chrisstarkmira15@gmail.com>
2025-07-26 22:50:58 -0400

    seo update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en'
       },
-      title: 'Friendly - Recognizing Friendly Faces.',
+      title: 'Recognizing Friendly Faces.',
       titleTemplate: '%s | Friendly',
       meta: [
         { charset: 'utf-8' },
